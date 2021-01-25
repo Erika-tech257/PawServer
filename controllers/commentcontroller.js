e162337084826d19093dedcda31dc8b5c6feef8b
ERika-tech257 <ErikaJtech@gmail.com>
2021-01-24 19:39:59 -0500

    added foreign key to comment model

--- controllers/commentcontroller.js
+++ controllers/commentcontroller.js
@@ -2,21 +2,23 @@ const express = require('express');
 const router = express.Router();
 const comments = require('../db').import('../models/comments');
 const validateSession = require('../middleware/validate-session');
+const pawPost = require('../db').import('../models/pawpost');
+const user = require('../db').import('../models/user');
 
 // create a comment under post
-router.post('/newComment', validateSession, (req, res) => {
+router.post('/new/comment', validateSession, (req, res) => {
     const reply = {
         description: req.body.comments.description, 
-        userId: req.body.user
+        owner: req.user.id
     }
     comments.create(reply)
     .then(comments => res.status(200).json(comments))
     .catch(err => res.status(500).json({error: err}))
 })
 
-// get user comment
+// get comments
 
-router.get('/newComment', validateSession, (req, res) => {
+router.get('/my/:id', validateSession, (req, res) => {
     comments.findOne({
         where: {
             userId: req.user.id
@@ -32,7 +34,7 @@ router.get('/newComment', validateSession, (req, res) => {
 
 // update comment
 
-router.put('/newComment', validateSession, (req, res) => {
+router.put('/comment', validateSession, (req, res) => {
     comments.update(req.body.comments.userName, { where: {userId: req.user.id}})
 
     .then(function commentUpdated(data) {
@@ -43,7 +45,7 @@ router.put('/newComment', validateSession, (req, res) => {
     }) .catch(err => res.status(500).json('User comment not updated', err))
 })
 
-router.delete('/newComment', validateSession, (req, res) => {
+router.delete('/comment', validateSession, (req, res) => {
     comments.destroy(req.body.comments.userName, { where: {userId: req.user.id}})
 
     .then(function commentUpdated(data) {
--- models/comments.js
+++ models/comments.js
@@ -1,6 +1,7 @@
 const  PawPost  = require('../db').import('./pawpost')
 const  User = require('../db').import('./user')
 const database = require('../db')
+
 module.exports = (sequelize, DataTypes) => {
     const Comments = sequelize.define('comments', {
         description: {
@@ -9,8 +10,8 @@ module.exports = (sequelize, DataTypes) => {
         }
     })
     // foreignKey: 'owner'
-    Comments.belongsTo(User, {as: 'reply'});
-    Comments.belongsTo(PawPost, {as:'PawPost'})
+    Comments.belongsTo(User, {as: 'reply', foreignKey:'owner'});
+    Comments.belongsTo(PawPost, {as:'PawPost'})  //In PG Admin it is PawPostId
   
     return Comments;
 }
