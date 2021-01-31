64dafcc8e8086cf041f5485dd3d6f396ecdf7747
ERika-tech257 <ErikaJtech@gmail.com>
2021-01-31 15:28:06 -0500

    added dialectoptions to db

--- db.js
+++ db.js
@@ -3,7 +3,13 @@ const Sequelize = require('sequelize');
 const database = new Sequelize(process.env.DATABASE_URL || `postgresql://postgres
 :${encodeURIComponent(process.env.PASS)}@localhost/PawServer`,
     {
-        dialect: 'postgres'
+        dialect: 'postgres',
+        dialectOptions: {
+            ssl: {
+              require: true,
+              rejectUnauthorized: false, // <<<<<<< YOU NEED THIS TO FIX UNHANDLED REJECTION 
+            },
+          },
     });
 
 
