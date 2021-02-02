8696e736d318c87a1985fc74551c6fe7f6bc886c
ERika-tech257 <ErikaJtech@gmail.com>
2021-02-02 17:16:38 -0500

    reset database server

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use('/comments', comments);
 
 db.authenticate()
 .then(() => db.sync({alter: true}))
-// alter:true checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model.
+// alter:true checks what is the current state of the table in the database (which columns it has, what are their data types, etc), and then performs the necessary changes in the table to make it match the model. force: true resets all tables
 .then(() => {
 app.listen(process.env.PORT,()=> {
     console.log(`App is listening on port ${process.env.PORT}`)})
