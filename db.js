84d836e643fd49d1ddc395ebc1ba5ece657dfc3e
ERika-tech257 <ErikaJtech@gmail.com>
2021-02-03 18:47:57 -0500

    heroku push

--- db.js
+++ db.js
@@ -3,7 +3,7 @@ const Sequelize = require('sequelize');
 const database = new Sequelize(process.env.DATABASE_URL,
     {
         dialect: 'postgres',
-        dialectOptions: process.env.DATABASE_URL.includes("postgres")? {}: {
+        dialectOptions: process.env.DATABASE_URL.includes("localhost")? {}: {
             ssl: {
               require: true,
               rejectUnauthorized: false, // <<<<<<< YOU NEED THIS TO FIX UNHANDLED REJECTION 
